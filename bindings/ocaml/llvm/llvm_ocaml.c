741bc98c760b62949e0d230dafd3bb443d0a2938
Yoonseung Kim <yoonseung.kim@sf.snu.ac.kr>
2015-10-23 18:20:54 +0900

    Add initialization for caml_alloc_small in llvm_struct_name.

--- bindings/ocaml/llvm/llvm_ocaml.c
+++ bindings/ocaml/llvm/llvm_ocaml.c
@@ -373,6 +373,7 @@ CAMLprim value llvm_struct_name(LLVMTypeRef Ty)
   if (C) {
     CAMLlocal1(result);
     result = caml_alloc_small(1, 0);
+    Field(result,0) = Val_int(0);
     Store_field(result, 0, caml_copy_string(C));
     CAMLreturn(result);
   }
