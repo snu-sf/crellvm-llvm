054231d755a6116397f0ea151ff92b36514312b1
markshin <shin.dongyeon@gmail.com>
2016-04-21 15:05:37 +0900

    trying constant case

--- lib/Transforms/InstCombine/InstCombinePHI.cpp
+++ lib/Transforms/InstCombine/InstCombinePHI.cpp
@@ -678,16 +678,123 @@ Instruction *InstCombiner::FoldPHIArgOpIntoPHI(PHINode &PN) {
   }
 
   Value *PhiVal;
+
+  llvmberry::ValidationUnit::GetInstance()->intrude(
+          [&PN, &InVal, &NewPN, &ConstantOp, &PhiVal](llvmberry::ValidationUnit::Dictionary &data,
+                                             llvmberry::CoreHint &hints) {
+            std::string oldphi = llvmberry::getVariable(PN);
+            BasicBlock::iterator InsertPos = PN.getParent()->getFirstInsertionPt();
+            llvmberry::insertSrcNopAtTgtI(hints, InsertPos);
+
+            PROPAGATE(   //from PN to insertPos propagate z in maydiff
+                    llvmberry::ConsMaydiff::make(oldphi, llvmberry::Physical),
+                    BOUNDS(PHIPOSJustPhi(SRC, PN), INSTPOS(TGT, InsertPos)));
+
   if (InVal) {
     // The new PHI unions all of the same values together.  This is really
     // common, so we handle it intelligently here for compile-time speed.
+    for (unsigned i = 0, e = PN.getNumIncomingValues(); i != e; ++i) {
+      Instruction *InInst = cast<Instruction>(PN.getIncomingValue(i));
+      std::string reg = llvmberry::getVariable(*InInst);
+
+      BinaryOperator *BinOp = cast<BinaryOperator>(InInst);
+      llvmberry::TyBop bop = llvmberry::getBop(BinOp->getOpcode());
+
+      Value *SpecialOperand = InInst->getOperand(0);
+      std::string reg_block_special = llvmberry::getVariable(*SpecialOperand);
+      std::string newphi = llvmberry::getVariable(*NewPN);
+
+
+      PROPAGATE( //from I to endofblock propagate x or y depend on edge
+              LESSDEF(VAR(reg, Physical),
+                      RHS(reg, Physical, SRC), SRC),
+              BOUNDS(INSTPOS(SRC, InInst),
+                     llvmberry::TyPosition::make_end_of_block(SRC, *(InInst->getParent()))));
+
+      //z = x^ -> z = x
+      INFRULE(PHIPOS(SRC, PN, InInst),
+              llvmberry::ConsTransitivity::make(
+                      VAR(oldphi, Physical), VAR(reg, Previous),
+                      VAR(reg, Physical)));
+
+      // z = x -> z = a + b
+      INFRULE(PHIPOS(SRC, PN, InInst),
+              llvmberry::ConsTransitivity::make(
+                      VAR(oldphi, Physical), VAR(reg, Physical),
+                      INSN(BINOP(bop, TYPEOF(BinOp), VAL(BinOp->getOperand(0), Physical),
+                                 VAL(BinOp->getOperand(1), Physical)))));
+
+      // { z >= a + b } at src after phinode
+      PROPAGATE(
+              LESSDEF(VAR(oldphi, Physical),
+                      RHS(reg, Physical, SRC), SRC),
+              BOUNDS(PHIPOSJustPhi(SRC, PN), INSTPOS(SRC, InsertPos)));
+      }
+
+
     PhiVal = InVal;
     delete NewPN;
   } else {
+
+                for (unsigned i = 0, e = PN.getNumIncomingValues(); i != e; ++i) {
+                Instruction *InInst = cast<Instruction>(PN.getIncomingValue(i));
+                std::string reg = llvmberry::getVariable(*InInst);
+                BinaryOperator *BinOp = cast<BinaryOperator>(InInst);
+                llvmberry::TyBop bop = llvmberry::getBop(BinOp->getOpcode());
+
+                Value *SpecialOperand = InInst->getOperand(0);
+                std::string reg_block_special = llvmberry::getVariable(*SpecialOperand);
+                std::string newphi = llvmberry::getVariable(*NewPN);
+
+
+                PROPAGATE( //from I to endofblock propagate x or y depend on edge
+                        LESSDEF(VAR(reg, Physical), RHS(reg, Physical, SRC), SRC),
+                        BOUNDS(INSTPOS(SRC, InInst),
+                               llvmberry::TyPosition::make_end_of_block(SRC, *(InInst->getParent()))));
+
+                // x^ >= a^+ const , z = x^ -> z >= a^ + const
+                INFRULE(PHIPOS(SRC, PN, InInst),
+                        llvmberry::ConsTransitivity::make(
+                                VAR(oldphi, Physical), VAR(reg, Previous),
+                                INSN(BINOP(bop, TYPEOF(SpecialOperand), VAL(SpecialOperand, Previous),
+                                           VAL(ConstantOp, Physical)))));
+
+                // introduce a^ >= k && k >= a^
+                INFRULE(PHIPOS(TGT, PN, InInst),
+                        llvmberry::ConsIntroGhost::make(VAL(SpecialOperand, Previous), REGISTER("K", Ghost)));
+
+                // infer k >= a^ && a^ >= t -> k >= t in tgt
+                INFRULE(PHIPOS(TGT, PN, InInst),
+                        llvmberry::ConsTransitivityTgt::make(VAR("K", Ghost), EXPR(SpecialOperand, Previous),
+                                                             VAR(newphi, Physical)));
+
+                // infer z = a^ + const -> z >= K + const in src
+                INFRULE(PHIPOS(SRC, PN, InInst),
+                        llvmberry::ConsReplaceRhs::make(
+                                REGISTER(reg_block_special, Previous), ID("K", Ghost), VAR(oldphi, Physical),
+                                INSN(BINOP(bop, TYPEOF(SpecialOperand), VAL(SpecialOperand, Previous),
+                                           VAL(ConstantOp, Physical))),
+                                INSN(BINOP(bop, TYPEOF(SpecialOperand), ID("K", Ghost),
+                                           VAL(ConstantOp, Physical)))));
+
+                // { z >= K + const } at src after phinode
+                PROPAGATE(LESSDEF(VAR(oldphi, Physical),
+                                  INSN(BINOP(bop, TYPEOF(SpecialOperand), ID("K", Ghost),
+                                             VAL(ConstantOp, Physical))), SRC),
+                          BOUNDS(PHIPOSJustPhi(SRC, PN), INSTPOS(SRC, InsertPos)));
+
+                // { K  >= t } at tgt after phinode
+                PROPAGATE(LESSDEF(VAR("K", Ghost),
+                                  VAR(newphi, Physical), TGT),
+                          BOUNDS(PHIPOSJustPhi(TGT, PN), INSTPOS(TGT, InsertPos)));
+
+
+              }
+
     InsertNewInstBefore(NewPN, PN);
     PhiVal = NewPN;
   }
-
+});
   // Insert and return the new operation.
   if (CastInst *FirstCI = dyn_cast<CastInst>(FirstInst)) {
     CastInst *NewCI = CastInst::Create(FirstCI->getOpcode(), PhiVal,
